d4125d35a3d5c9945f71ec902293fc70efdf3f24
Adrian <adrianbueno095@gmail.com>
2023-12-04 16:55:38 +0100

    add more code

---
+++ js/src/main.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById("start-game")
+const showRules = document.getElementById("showRules")
+
+startButton.addEventListener("click", () => {
+
+})
+
+
+showRules.addEventListener("click", () => {
+
+})
