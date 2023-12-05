e1901d512a2276805ed39c8f4ec6a447b97bbeeb
Adrian <adrianbueno095@gmail.com>
2023-12-05 13:19:39 +0100

    add main screen state, game board state

--- js/src/main.js
+++ js/src/main.js
@@ -1,11 +1,40 @@
-const startButton = document.getElementById("start-game")
-const showRules = document.getElementById("showRules")
+const startButton = document.getElementById("start-game");
+const showRulesButton = document.getElementById("showRules");
+const closeRulesButton = document.getElementById("closeRules");
+const returnToStartWinButton = document.getElementById("returnToStartWin");
+const returnToStartLoseButton = document.getElementById("returnToStartLose");
+
+const startScreen = document.getElementById("app");
+const rulesModal = document.getElementById("rulesModal");
+const winPage = document.getElementById("winPage");
+const losePage = document.getElementById("losePage");
+
+
+window.addEventListener("load", () => {
+    startScreen.classList.remove("hidden");
+});
 
 startButton.addEventListener("click", () => {
+    startScreen.classList.add("hidden");
 
-})
+});
 
+showRulesButton.addEventListener("click", () => {
+    rulesModal.classList.remove("hidden");
+});
 
-showRules.addEventListener("click", () => {
+closeRulesButton.addEventListener("click", () => {
+    rulesModal.classList.add("hidden");
+});
 
-})
+returnToStartWinButton.addEventListener("click", () => {
+    winPage.classList.add("hidden");
+    startScreen.classList.remove("hidden");
+
+});
+
+returnToStartLoseButton.addEventListener("click", () => {
+    losePage.classList.add("hidden");
+    startScreen.classList.remove("hidden");
+
+});
