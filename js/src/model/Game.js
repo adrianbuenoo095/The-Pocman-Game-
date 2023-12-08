fd6de0552fb93b19bbcba9282cb2f6ae90b7cb88
Adrian <adrianbueno095@gmail.com>
2023-12-08 13:28:25 +0100

    refactor game class and fix bugs

--- js/src/model/Game.js
+++ js/src/model/Game.js
@@ -8,6 +8,7 @@ class Game {
         this.score = 0;
         this.lives = 5;
         this.gameState = "playing";
+        this.isGameOver = false;
     }
 
     start() {
@@ -15,8 +16,7 @@ class Game {
         this.endScreen.style.display = 'none'
         this.gameScreen.style.display = 'block'
         this.spawnObstacles();
-        this.updateScore(0);
-        this.updateLives(5);
+
         this.gameState = "playing";
         this.player = new Player(this.gameScreen)
         this.gameLoop();
@@ -32,23 +32,48 @@ class Game {
     }
 
     gameLoop() {
-        console.log('animate')
-        this.player.move()
+        console.log('animate');
+        this.player.move();
+
+        const nextObstacles = [];
         this.obstacles.forEach(currentObstacle => {
-            currentObstacle.move()
-            this.player.didCollide(currentObstacle)
-        })
-        requestAnimationFrame(() => this.gameLoop())
+            currentObstacle.move();
+
+            if (currentObstacle.top + currentObstacle.height < 0) {
+                currentObstacle.element.remove();
+            } else {
+                if (this.player.didCollide(currentObstacle)) {
+                    console.log('collision');
+                    this.lives -= 1;
+                    if (this.lives <= 0) {
+                        this.isGameOver = true;
+                    }
+                } else {
+                    nextObstacles.push(currentObstacle);
+                }
+            }
+        });
+
+        this.obstacles = nextObstacles;
+
+        if (this.animateId % 300 === 0) {
+            this.obstacles.push(new Obstacle(this.gameScreen));
+        }
+
+        console.log(this.obstacles);
+
+        document.getElementById('score').innerText = this.score;
+        document.getElementById('lives').innerText = this.lives;
+
+        if (this.isGameOver) {
+            this.gameScreen.style.display = 'none';
+            this.endScreen.style.display = 'block';
+            this.player.element.remove();
+        } else {
+            console.log(this.animateId);
+            this.animateId = requestAnimationFrame(() => this.gameLoop());
+        }
     }
 
-    updateScore(points) {
-        this.score += points;
-    }
 
-    updateLives(lives) {
-        this.lives = lives;
-    }
-
-    checkCollision() {
-    }
 }
